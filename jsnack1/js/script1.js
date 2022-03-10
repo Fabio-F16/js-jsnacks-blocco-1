1b2d9c3a3335d2c6b795660dea14371192c67db6
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-10 15:58:46 +0100

    file

---
+++ jsnack1/js/script1.js
@@ -0,0 +1,22 @@
+console.log('js ok')
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+const invitati = ['elena', 'Marco', 'Alberto', 'Daniele', 'Diego', 'Fabio', 'Francesca']
+console.log(invitati)
+
+const yourName = prompt('Come ti chiami?').value
+
+
+
+if(invitati.lenght > yourName){
+    console.log('presente in listacc')
+}
+
+
+
+
+
+
---
+++ jsnack3/js/script.js
@@ -0,0 +1,23 @@
+console.log('js ok')
+
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+
+const invitati = ['Elena', 'Marco', 'Alberto', 'Daniele', 'Diego', 'Fabio', 'Francesca']
+console.log(invitati)
+
+const yourName = prompt('Come ti chiami?').value
+
+
+
+if(invitati.lenght ){
+
+}
+
+
+
+
+
+
