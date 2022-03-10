9284a8d4c93df5a0f382234624c23943bf960789
Fabio-F16 <fipsfabio95@gmail.com>
2022-03-10 16:49:04 +0100

    esercizio uno

--- jsnack1/js/script1.js
+++ jsnack1/js/script1.js
@@ -4,19 +4,27 @@ console.log('js ok')
 // chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 
-const invitati = ['elena', 'Marco', 'Alberto', 'Daniele', 'Diego', 'Fabio', 'Francesca']
-console.log(invitati)
-
-const yourName = prompt('Come ti chiami?').value
-
+const invitati = ['elena', 'Marco', 'Alberto', 'Daniele', 'Diego', 'Fabio', 'Francesca'];
+const yourName = prompt('Come ti chiami?');
+let presente = false;
 
 
-if(invitati.lenght > yourName){
-    console.log('presente in listacc')
+if(yourName){
+
+    for (let i = 0; i < invitati.length; i ++){
+        const validName = invitati[i];
+
+        if(yourName === validName){
+            presente = true;
+        }   
+    }
 }
 
+if (presente){
+    console.log('Sei stato invitato! Puoi accedere');
+}else{
+    console.log('Non sei stato invitato, mi dispiace :-(');
+}
 
-
-
-
-
+ 
+        
